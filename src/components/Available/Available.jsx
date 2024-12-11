e6b775969c50a82c06edcc42a36fc54e5024474a
miti <muntajimamahbub5@gmail.com>
2024-12-11 07:45:47 +0600

    Hire your favourite players

--- src/components/Available/Available.jsx
+++ src/components/Available/Available.jsx
@@ -5,7 +5,7 @@ const Available = ({selected, handleIsActiveState, isActive}) => {
     return (
         <div className="mt-44">
             <div className="flex justify-between">
-                <h3>Available Players</h3>
+                <h3 className="text-xl font-bold">Available Players</h3>
                 <div className="flex overflow-hidden border-2 border-[#ccc] rounded-xl">
                     <button onClick={() => handleIsActiveState("available")} className={`${isActive.status == "available" ? " bg-yellow-400 border-none flex-1 py-2 px-6" : "border-none flex-1 py-2 px-6"}`}>Available</button>
                     <button onClick={() => handleIsActiveState("selected")} className={`${isActive.status == "selected" ? "bg-yellow-400 border-none py-2 px-6" : "border-none py-2 px-6"}`}>Selected {selected.length} </button>
